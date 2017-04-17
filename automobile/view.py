366f68b480dd5e8a890ed2aa71356b85f8de7684
wangxr14 <wangxr1108@126.com>
2017-04-17 17:09:10 +0800

    Check price result

--- automobile/view.py
+++ automobile/view.py
@@ -231,7 +231,7 @@ def renderResult(request):
 		
 		resultlist = json.loads(result).get("Results").get("output1").get("value").get("Values")[0]
 		length = len(resultlist)
-		result = resultlist[length-1]
+		result = resultlist[length-1]+'   '+resultlist[length-2]
 		
 	except urllib2.HTTPError, error:
 		print("The request failed with status code: " + str(error.code))
