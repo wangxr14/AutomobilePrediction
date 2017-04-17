a8b41fe7f77fa9e32e9ec7cc40dd54bb6053906b
wangxr14 <wangxr1108@126.com>
2017-04-17 17:37:38 +0800

    Change Request

--- automobile/view.py
+++ automobile/view.py
@@ -206,7 +206,7 @@ def renderResult(request):
 					"input1":
 					{
 						"ColumnNames": ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"],
-						"Values": [ [ "0", "0", make, "value", "value", "value", bodystyle, "value", "value", wheelbase, "0", "0", "0", "0", "value", "value", enginesize, "value", "0", "0", "0", horsepower, peakrpm, "0", highwaympg, price ],  ]
+						"Values": [ [ "0", "0", "value", "value", "value", "value", "value", "value", "value", "0", "0", "0", "0", "0", "value", "value", "0", "value", "0", "0", "0", "0", "0", "0", "0", "0" ], [ "0", "0", make, "value", "value", "value", bodystyle, "value", "value", wheelbase, "0", "0", "0", "0", "value", "value", enginesize, "value", "0", "0", "0", horsepower, peakrpm, "0", highwaympg, price ],]
 					},        },
 				"GlobalParameters": {
 	}
